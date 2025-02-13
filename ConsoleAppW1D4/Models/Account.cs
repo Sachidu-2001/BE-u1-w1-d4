6c954cfa2fd1c6819c72f9bf30290c2264fd3bee
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-13 16:46:03 +0100

    commit

---
+++ ConsoleAppW1D4/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppW1D4.Models
+{
+    public class Account
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+ 
+        public string Password { get; set; }
+    }
+}
--- ConsoleAppW1D4/Program.cs
+++ ConsoleAppW1D4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Seehttps://aka.ms/new-console-templateformoreinformation
+
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.WriteLine("Benvenuto nella pagina Login");
+
+
+Console.WriteLine("Vuoi creare un account (s/n)");
+
+var CreareAccount=Console.ReadLine();
+
+var InserisciNome = "";
+
+var InserisciCognome= "";
+
+
+if( CreareAccount == "s")
+{
+    Console.WriteLine("Perfetto creiamo account");
+
+
+
+}else if (CreareAccount == "n")
+{
+    Console.WriteLine("Arrivederci");
+        return;
+}
+
+
+
